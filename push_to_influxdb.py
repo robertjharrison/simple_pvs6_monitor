8c87a81c4a7677f89d4baff1e25eb67389668094
Robert J. Harrison <rjharrison@gmail.com>
2025-11-14 19:33:30 -0500

    working code pushing power, energy, temp for live data and inverters to local influxdb; json source and snapshot of basic grafana dashboard using the database

--- push_to_influxdb.py
+++ push_to_influxdb.py
@@ -5,44 +5,53 @@ import pvs_simple
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-bucket = "test3"
+bucket = "PVS"
 token = os.environ.get("INFLUXDB_TOKEN")
 org = "Hoime"
 url = "http://192.168.5.8:8086"
-# Store the URL of your InfluxDB instance                                                                                                                                                          
 
-client = influxdb_client.InfluxDBClient(
-    url=url,
-    token=token,
-    org=org
-)
-
+client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
-async def push_to_influxdb(interval=60):
-    '''                                                                                                                                                                                                
-    Template for data
 
-    timestamp
-      for device in system and all inverters
+def record_device(timestamp, device, energy, power, temp):
+    p = influxdb_client.Point("PVS6").tag("device", device).time(timestamp).field("kWh", energy).field("kW", power).field("C",temp)
+    write_api.write(bucket=bucket, org=org, record=p)
+
+async def push_to_influxdb(interval=60):
+    '''                                                                                                                                    Template for data
+
+    for device in livedata and all inverters
       tag device
         field kWh lifetime energy
         field kW current power
         field C  current temperature
     '''
-    
+
     async with aiohttp.ClientSession() as session:
         pvs = pvs_simple.PVS(session)
         await pvs.initialize()
         while True:
-            data = await pvs._post(data="match=/sys")
-            inverters = pvs_simple.Inverters(data)
-            livedata = pvs_simple.LiveData(data)
-            print(livedata.get_data())
-            for id in range(inverters.num_devices()):
-                print(inverters.get_data(id))
+            data = await pvs.post(data="match=/sys")
 
-            await asyncio.sleep(interval)
-    
+            livedata = pvs_simple.LiveData(data)
+            device = "system"
+            timestamp, energy, power = livedata.get_data()
+            temp = 0.0
+            record_device(timestamp, device, energy, power, temp)            
+            print(timestamp, f"Recorded system: kWh={energy} kW={power}")
+            system_power = power
+
+            inverters = pvs_simple.Inverters(data)
+            for inverter in range(inverters.num_devices()):
+                device = f"inverter-{inverter:02d}"
+                energy, power, temp = inverters.get_data(inverter)
+                record_device(timestamp, device, energy, power, temp)
+
+            sleep_interval = interval
+            if system_power <= 20:
+                sleep_interval = min(interval, 1200) # check less often at night
+            await asyncio.sleep(sleep_interval)
 
 if __name__ == "__main__":
-    asyncio.run(push_to_influxdb(10))
+    # The live data seems to be updated in real time but the inverter data only updates every 5 minutes.
+    asyncio.run(push_to_influxdb(60))
